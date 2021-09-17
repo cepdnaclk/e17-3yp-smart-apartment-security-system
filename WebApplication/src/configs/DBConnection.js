1c864ea37fdfc2bc8eed0ebb1056f07cd543c7a4
1997Nisansala <dananjaya.nisansale@gmail.com>
2021-09-18 00:38:54 +0530

    update homepage and apartment page

--- WebApplication/src/configs/DBConnection.js
+++ WebApplication/src/configs/DBConnection.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+//require('dotenv').config();
 import mysql from "mysql2";
 
 let connection = mysql.createConnection({
--- WebApplication/src/controllers/apartments.js
+++ WebApplication/src/controllers/apartments.js
@@ -1,9 +1,33 @@
+import DBConnection from "./../configs/DBConnection";
+
 let apartments = async (req, res) => {
-    return res.render("apartments.ejs",{
-        user: req.user
+    let sql = "SELECT * FROM apartments";
+    let query = DBConnection.query(sql, (err, rows) => {
+        if(err) throw err;
+        res.render('apartments', {
+            users : rows
+        });
     });
 };
 
+let apartmentregister = (req, res) => {
+    return res.render("newapartment", {
+        errors: req.flash("errors")
+    });
+};
+
+let saveapartmentdetail = async (req, res) => { 
+    let data = {name: req.body.name, houseno: req.body.houseno, owner: req.body.owner, mobileno: req.body.mobileno, location: req.body.location};
+    let sql = "INSERT INTO apartments SET ?";
+    let query = DBConnection.query(sql, data,(err, results) => {
+      if(err) throw err;
+      res.redirect('/apartment');
+    });
+};
+
+
 module.exports = {
     apartments : apartments,
+    apartmentregister : apartmentregister,
+    saveapartmentdetail : saveapartmentdetail,
 };
